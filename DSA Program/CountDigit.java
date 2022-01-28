d39c17966647ed29615f2f52f7f17e12a8bc78ae
Ishwar870 <64201359+Ishwar870@users.noreply.github.com>
2022-01-29 01:35:50 +0530

    Create CountDigit.java

---
+++ DSA Program/CountDigit.java
@@ -0,0 +1,16 @@
+package Maths;
+
+import java.util.Scanner;
+
+public class CountDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        int x = sc.nextInt();
+        int res = 0;
+        while(x > 0){
+            x = x/10;
+            res++;
+        }
+        System.out.print(res);
+    }
+}
