281ec6626e1a01c78c220e4c21591ac17572a7bb
Ishwar Chand <64201359+Ishwar870@users.noreply.github.com>
2022-04-01 21:41:26 +0530

    Add files via upload

---
+++ Udemy/src/LinkedList/ReturnNthToLast.java
@@ -0,0 +1,17 @@
+package InterViewLinkedList;
+
+public class ReturnNthToLast {
+    public Node nthToLast(LinkedList ll , int n){
+        Node p1 = ll.head;
+        Node p2 = ll.head;
+        for (int i=0;i<n;i++){
+            if (p2 == null) return null;
+            p2 = p2.next;
+        }
+        while (p2 != null){
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p1;
+    }
+}
