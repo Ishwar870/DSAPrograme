224850828133e7974c136cc4ee96312544134ece
Ishwar Chand <64201359+Ishwar870@users.noreply.github.com>
2022-04-02 22:04:27 +0530

    Add files via upload

--- Udemy/src/LinkedList/Main.java
+++ Udemy/src/LinkedList/Main.java
@@ -1,19 +1,33 @@
-package LinkedList;
+package InterViewLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        SingleLinkedList sll = new SingleLinkedList();
-        sll.createLinkedList(5);
-//        sll.createLinkedList(6);
-        sll.insertLinkedList(10,1);
-        sll.insertLinkedList(15,2);
-        sll.insertLinkedList(20,3);
-        sll.insertLinkedList(25,4);
-//        System.out.println(sll.head.value);
-        sll.Traversal();
-//        sll.searchNode(24);
-        sll.deleteSll();
-        sll.Traversal();
+//        LinkedList ll = new LinkedList();
+//        ll.createLL(1);
+//        ll.insert(9);
+//        ll.insert(5);
+//        ll.insert(10);
+//        ll.insert(2);
+//        ll.traversel();
+//        RemoveDuplicate RD = new RemoveDuplicate();
+//        ReturnNthToLast N = new ReturnNthToLast();
+//        Partition P = new Partition();
+//        RD.deleteDuplicate(ll);
+//        ll.traversel();
+//       Node n = N.nthToLast(ll,2);
+//        System.out.println(n.value);
+//        LinkedList t = P.partition(ll,6);
+//        t.traversel();
+        LinkedList llA = new LinkedList();
+        llA.insert(7);
+        llA.insert(1);
+        llA.insert(6);
+        LinkedList llB = new LinkedList();
+        llB.insert(5);
+        llB.insert(9);
+        llB.insert(2);
+        SumOfLinkedList sum = new SumOfLinkedList();
+        LinkedList result = sum.sumlist(llA,llB);
+        result.traversel();
     }
-//    Total time complexity O(1) and space complexity O(1)
 }
