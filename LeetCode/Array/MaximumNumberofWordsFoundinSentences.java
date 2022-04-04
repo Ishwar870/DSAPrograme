9e769ba0f55af0c78831f116ad09a8164e2e4c6d
Ishwar Chand <64201359+Ishwar870@users.noreply.github.com>
2022-04-04 23:01:04 +0530

    Update MaximumNumberofWordsFoundinSentences.java

--- LeetCode/Array/MaximumNumberofWordsFoundinSentences.java
+++ LeetCode/Array/MaximumNumberofWordsFoundinSentences.java
@@ -3,8 +3,21 @@ package Arrays;
 public class MaximumNumberofWordsFoundinSentences {
     public static int mostWordsFound(String[] sentences){
         int max = 0;
+//         Method 1
         for (String word: sentences){
             max = Math.max(word.split(" ").length,max);
+        }
+//         method 2
+        for (String sentence : sentences){
+            int count = 1;
+            for (int i =0;i<sentence.length();i++){
+                if (sentence.charAt(i) == ' ' ){
+                    count++;
+                }
+                if (max < count){
+                    max = count;
+                }
+            }
         }
         return max;
     }
