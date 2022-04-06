1ac2516c2f33d7ebe259a127fbb59de14deb8ddb
Ishwar Chand <64201359+Ishwar870@users.noreply.github.com>
2022-04-06 20:08:21 +0530

    Update ShuffleStringArray.java

--- LeetCode/Array/ShuffleStringArray.java
+++ LeetCode/Array/ShuffleStringArray.java
@@ -9,8 +9,9 @@ public class ShuffleStringArray {
         return String.valueOf(result);
     }
     public static void main(String[] args) {
-        String s = "codeleet";
-        int index[] = {4,5,6,7,0,2,1,3};
+        Scanner sc = new Scanner(System.in;
+        String s = "warhsi";
+        int index[] = {3,4,5,2,1,0};
         restore(s,index);
         System.out.println(restore(s,index));
     }
